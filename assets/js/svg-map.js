47e6feabb463b537b9b392c992265e45f67b122c
degand <pdegand@mailinator.com>
2025-10-22 11:01:50 +0200

    Updated from Publii

--- assets/js/svg-map.js
+++
@@ -1,7 +0,0 @@
-window.publiiSvgFix = {
-
-    "#search": {
-        "viewbox": "0 0 24 24",
-        "content": "<circle cx=\"11\" cy=\"11\" r=\"8\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" /><path d=\"M21 21l-4.3-4.3\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" />"
-    }
-};
